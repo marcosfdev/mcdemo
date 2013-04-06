0c111de814d0d330ba9fabb004aec4aa93e29c1a
Marc <marcobrien12@gmail.com>
2013-04-06 16:31:53 -0700

    m calc script

---
+++ mcal.rb
@@ -0,0 +1,13 @@
+print "Enter Loan Amount:"
+loan = gets.chomp.to_i
+print "Enter Length of time in months:"
+time = gets.chomp.to_i
+print "Enter interest rate:"
+rate = gets.chomp.to_f/100
+
+i = (1+rate/12)**(12/12)-1
+annuity = (1-(1/(1+i))**time)/i
+
+payment = loan/annuity
+
+puts "\n$%.2f per month" % [payment]
